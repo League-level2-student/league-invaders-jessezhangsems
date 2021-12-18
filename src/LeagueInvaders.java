bf3ec67f33311baffaa8e4cda45cc5e60b78e904
student <student@ip-172-31-62-195.us-west-2.compute.internal>
2021-12-17 17:00:20 -0800

    League Invaders and Game Panel

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,10 +1,28 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
-	public static final int width(500);
+	public static final int width=500;
+	public static final int height=800; 
+	JFrame frame; 
+	GamePanel panel;
 public static void main(String[] args) {
-	JFrame frame=new JFrame(); 
+	LeagueInvaders leagueinvaders=new LeagueInvaders();
 	
 }
 
+LeagueInvaders(){
+	frame=new JFrame();
+	panel=new GamePanel();
+	
+	setup();
+	
+}
+void setup(){
+	frame.setVisible(true); 
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(width,height);
+	frame.setTitle("League Invaders Program");
+	frame.add(panel);
+	
+}
 }
